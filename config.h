e849e2ac0da6890721908580ae29ccaa7dc06253
dongguo liyan <1216582461@qq.com>
2023-11-13 08:44:43 +0800

    Add files via upload
    build&add: 创建并添加例程代码

---
+++ config.h
@@ -0,0 +1,19 @@
+#ifndef		__CONFIG_H
+#define		__CONFIG_H
+
+//#define MAIN_Fosc		22118400L	//定义主时钟
+//#define MAIN_Fosc		12000000L	//定义主时钟
+//#define MAIN_Fosc		11059200L	//定义主时钟
+//#define MAIN_Fosc		 5529600L	//定义主时钟
+#define MAIN_Fosc		24000000L	//定义主时钟
+
+#include "stc8g.h"
+
+typedef 	unsigned char	u8;
+typedef 	unsigned int	u16;
+typedef 	unsigned long	u32;
+
+#define	TRUE	1
+#define	FALSE	0
+
+#endif
---
+++ main.c
@@ -0,0 +1,70 @@
+/*****************************************************************************************
+ *   Pin connection:              
+ *                 ___
+ * LED  ->  P5.4 -|   |- P3.3  SCL
+ * 5.0V ->   VCC -|   |- P3.2  SDA
+ *  ADC ->  P5.5 -|   |- P3.1  PWM1
+ *  GND ->   GND -|___|- P3.0  PWM0
+ *   test-MCU: STC8G1K08A
+ * P54 : BOOT
+ * P55 : ADC
+ * P33 : SCL
+ * P32 : SDA
+ * P31 : PWM1/Tx
+ * P30 : PWM0/Rx
+ *****************************************************************************************/
+
+#include "lib/gpio.h"
+#include "lib/delay.h"
+#include "lib/uart.h"
+#include "lib/pwm.h"
+#include "lib/i2c.h"
+#include "lib/os.h"
+
+void i2c_task(void);
+void led_blink(void);
+void system_init(void);
+
+void main(void)
+{
+	void system_init();
+
+	while(1)
+	{
+		task_process();
+	}
+}
+
+void i2c_task(void)
+{
+	i2c_write_date(0x50, 0x00, "Hello World!", 1);
+}
+
+void led_blink(void)
+{
+	gpio_toggle(P54);
+}
+
+void system_init(void)
+{
+	gpio_init_allpin(3, GPIO_OUT_PP);
+	gpio_init_allpin(5, GPIO_OUT_PP);
+	pwm_init(3, 1, 20000, 1000);
+	pwm_init(3, 0, 20000, 20000-1000);
+	i2c_slave_init(I2C_P33_P32, 0x50, 0);
+	uart1_init();
+
+	os_timer0_init();
+	add_task(1, 3, 10, i2c_task);
+	add_task(1, 0, 500, led_blink);
+
+	delay_ms(1000);
+	print("Hello World!\r\n");
+	
+	gpio_set(P54, Pin_High);
+}
+
+
+
+
+
